c0b3033dbb61552362ef396b9543439e08038b27
Anastasia <achernova1551@gmail.com>
2022-05-02 22:04:34 +0300

    02.05

--- source/js/modules/main-nav.js
+++ source/js/modules/main-nav.js
@@ -1,5 +1,7 @@
 const navMain = document.getElementById('main-nav');
 const navToggle = document.getElementById('main-nav-toggle');
+const navWrapper = document.getElementById('nav-wrapper');
+const pageBody = document.getElementById('body');
 
 const getNavigation = () => {
   navMain.classList.remove('is-active');
@@ -9,10 +11,18 @@ const getNavigation = () => {
     if (navMain.classList.contains('is-invalid')) {
       navMain.classList.remove('is-invalid');
       navMain.classList.add('is-open');
+      pageBody.classList.add('scroll-hidden');
     } else {
       navMain.classList.add('is-invalid');
       navMain.classList.remove('is-open');
+      pageBody.classList.remove('scroll-hidden');
     }
+  });
+
+  navWrapper.addEventListener('click', function () {
+    navMain.classList.add('is-invalid');
+    navMain.classList.remove('is-open');
+    pageBody.classList.remove('scroll-hidden');
   });
 };
 
